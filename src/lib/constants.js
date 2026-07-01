22626a62f2320b512268bf315b6f6cb5a156b540
henriksiverhall <henrik.siverhall@gmail.com>
2026-07-01 16:34:43 +0200

    v2.0.45-dev: version bump

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -1,4 +1,4 @@
-export const APP_VERSION = 'v2.0.44-dev'
+export const APP_VERSION = 'v2.0.45-dev'
 export const WORKER_URL = 'https://tradelog-claude-api-dev.henrik-siverhall.workers.dev'
 
 export const YAHOO_SYMBOL_MAP = {
