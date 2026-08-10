170ae665af37d1da7ea8de720e57f6410b293e4f
henriksiverhall <henrik.siverhall@gmail.com>
2026-08-10 19:18:23 +0200

    merge staging→main: chore – importera react-grid-layout/react-resizable CSS

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client'
 import { AuthProvider } from './hooks/useAuth'
 import App from './App'
 import './styles/globals.css'
+import 'react-grid-layout/css/styles.css'
+import 'react-resizable/css/styles.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
